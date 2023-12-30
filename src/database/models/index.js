52b3c933d273530757570cb8fd7058d5678d0881
setiawannuha <setiawannuha123@gmail.com>
2023-12-31 00:55:04 +0700

    change sequelize config for vercel deployment 2

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -13,11 +13,13 @@ const db = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
+    ...config,
     dialect: 'postgres',
     dialectModule: require('pg')
   });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
     dialect: 'postgres',
     dialectModule: require('pg')
   });
